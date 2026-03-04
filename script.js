fcdd33ff5cea1fade7ae372ed7e0d53d67df7d51
Amro <amro.youssef@ymail.com>
2026-03-04 11:07:50 +0000

    Add cors

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ app.use((req, res, next) => {
 
 // app.use(cookieParser());
 app.use(express.json());
+app.use(cors());
 // app.use(express.static("public"));
 
 app.get("/moons", async (req, res) => {
